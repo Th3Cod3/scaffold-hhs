305cd12d74463412ec9750b7a3cdb5d799b43a17
Th3Cod3 <yefri@th3cod3.com>
2022-03-30 19:10:38 +0200

    Improve debug lib

--- lib/debug.h
+++ lib/debug.h
@@ -3,14 +3,22 @@
 
 #ifdef DEBUG_EN
 #include <util/delay.h>
-#define DEBUG_PIN PORTB7
-#define DEBUG_SIGNAL(delay)  \
-    DDRB |= _BV(DEBUG_PIN);  \
-    PORTB ^= _BV(DEBUG_PIN); \
-    _delay_us((int)(delay)); \
-    PORTB ^= _BV(DEBUG_PIN);
+#ifdef defined(__AVR_ATmega2560__)
+#define DEBUG_DDR DDRB
+#define DEBUG_PORT PORTB
+#define DEBUG_PIN PB7 // pin D13
+#elif defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328__)
+#define DEBUG_DDR DDRC
+#define DEBUG_PORT PORTC
+#define DEBUG_PIN PC1 // pin TX/D0
+#endif // DEBUG
+#define DEBUG_SIGNAL(delay)      \
+    DEBUG_DDR |= _BV(DEBUG_PIN); \
+    DEBUG_PORT ^= _BV(DEBUG_PIN);     \
+    _delay_us((int)(delay));     \
+    DEBUG_PORT ^= _BV(DEBUG_PIN);
 #else
 #define DEBUG_SIGNAL(delay)
 #endif
 
-#endif
+#endif // DEBUG
